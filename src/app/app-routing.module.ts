43d9305faf2ed634af435807e979a7d924094379
Liseth Giraldo <70545096+lisethgira@users.noreply.github.com>
2023-08-28 09:56:33 -0500

    se corrige ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'posts', loadChildren: () => import('./publicaciones/publicaciones.module').then(m => m.PublicacionesModule) }, { path: 'users', loadChildren: () => import('./usuarios/usuarios.module').then(m => m.UsuariosModule) }, { path: 'todos', loadChildren: () => import('./tareas/tareas.module').then(m => m.TareasModule) }, { path: 'comments', loadChildren: () => import('./comentarios/comentarios.module').then(m => m.ComentariosModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'todos', pathMatch: 'full' }, 
+  {path: 'posts', loadChildren: () => import('./publicaciones/publicaciones.module').then(m => m.PublicacionesModule) },
+  { path: 'users', loadChildren: () => import('./usuarios/usuarios.module').then(m => m.UsuariosModule) },
+  { path: 'todos', loadChildren: () => import('./tareas/tareas.module').then(m => m.TareasModule) },
+  { path: 'comments', loadChildren: () => import('./comentarios/comentarios.module').then(m => m.ComentariosModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
