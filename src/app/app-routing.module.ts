35a46e53330f01a38000540cb5dd203c26af888a
Liseth Giraldo <70545096+lisethgira@users.noreply.github.com>
2023-08-28 10:00:52 -0500

    fic: se hermosea codigo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'todos', pathMatch: 'full' }, 
-  {path: 'posts', loadChildren: () => import('./publicaciones/publicaciones.module').then(m => m.PublicacionesModule) },
+  { path: '', redirectTo: 'todos', pathMatch: 'full' },
+  { path: 'posts', loadChildren: () => import('./publicaciones/publicaciones.module').then(m => m.PublicacionesModule) },
   { path: 'users', loadChildren: () => import('./usuarios/usuarios.module').then(m => m.UsuariosModule) },
   { path: 'todos', loadChildren: () => import('./tareas/tareas.module').then(m => m.TareasModule) },
   { path: 'comments', loadChildren: () => import('./comentarios/comentarios.module').then(m => m.ComentariosModule) }];
